d65b576770cb20e2f0fa700cd2311ccf99b51b40
germmand <german.aguilera17@gmail.com>
2018-04-11 17:58:57 -0400

    agregando modelo de encargos

---
+++ src/app/models/EncargosModel.ts
@@ -0,0 +1,12 @@
+export class EncargoModel {
+    public EncargoId: number;
+    public Albaran: string;
+    public Destinatario: string;
+    public Direccion: string;
+    public Poblacion: string;
+    public CP: string;
+    public Provincia: string;
+    public Telefono: string;
+    public Observaciones: string;
+    public Fecha: string;
+};

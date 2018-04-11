6bd578186c17c61a05e5e44c621d312c1e54390f
germmand <german.aguilera17@gmail.com>
2018-04-10 22:04:14 -0400

    Agregando Modulos necesarios para la creación de la vista inicial.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatToolbarModule
     ],
     exports: [
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatToolbarModule
     ]
 })
 export class SharedModule {

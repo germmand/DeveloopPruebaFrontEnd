6029cccd8fc06af46fbcc3a7632875ed829e2774
germmand <german.aguilera17@gmail.com>
2018-04-12 19:28:22 -0400

    Agregando header a la vista de exportar datos.

--- src/app/components/exportboard/exportboard.component.ts
+++ src/app/components/exportboard/exportboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-exportboard',
@@ -6,11 +7,23 @@ import { Component } from '@angular/core';
     styleUrls: ['./exportboard.component.css']
 })
 export class ExportboardComponent {
+    private correctInputControl : FormControl;
+    private incorrectInputControl : FormControl;
+
     constructor() {
 
     }
 
     ngOnInit() {
+        this.correctInputControl = new FormControl();
+        this.incorrectInputControl = new FormControl();
+    }
+
+    OnKeyPress(event: Event): void {
+        event.preventDefault();
+    }
+
+    OnExportData(event: Event) {
 
     }
 }
